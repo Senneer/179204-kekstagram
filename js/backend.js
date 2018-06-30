d054edcd5db1aad37f5be8e8d27458a36cb8a349
Senneer <senneerr@gmail.com>
2018-06-30 13:40:56 +0700

    Добавил модуль backend для отправки/получения данных

---
+++ js/backend.js
@@ -0,0 +1,94 @@
+'use strict';
+
+(function () {
+
+  var Url = {
+    UPLOAD_IMG: 'https://js.dump.academy/kekstagram',
+    WIZARD_LIST: 'https://js.dump.academy/kekstagram/data'
+  };
+
+  function errorHandler(onError) {
+    onError('Произошла ошибка соединения');
+  }
+
+  function timeoutHandler(onError, time) {
+    onError('Запрос не успел выполниться за ' + time / 1000 + ' сек.');
+  }
+
+  function renderErrorNode(message) {
+    var TIME_TO_SHOW = 6000;
+    var existingError = document.querySelector('.error');
+
+    var node = document.createElement('div');
+    node.classList = 'error';
+    node.style = 'position: fixed; right: 20px; bottom: 20px; max-width: 400px; padding: 10px; font-size: 16px; text-transform: none; color: #000; background: #fff; border-right: 4px solid #ff4d4d; z-index: 3;';
+    node.textContent = message;
+
+    if (existingError) {
+      existingError.remove();
+    }
+    document.body.insertAdjacentElement('beforeend', node);
+
+    setTimeout(function () {
+      node.remove();
+    }, TIME_TO_SHOW);
+  }
+
+  function load(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler(onError);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      timeoutHandler(onError, xhr.timeout);
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', Url.WIZARD_LIST);
+    xhr.send();
+  }
+
+  function upload(data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler(onError);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      timeoutHandler(onError, xhr.timeout);
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('POST', Url.UPLOAD_IMG);
+    xhr.send(data);
+  }
+
+  window.backend = {
+    load: load,
+    upload: upload,
+    showError: renderErrorNode
+  };
+})();
--- js/gallery.js
+++ js/gallery.js
@@ -5,31 +5,6 @@
   var pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture__link');
-  var otherPeoplePictures = [];
-
-  function generatePictureUrl(id) {
-    var url = 'photos/' + id + '.jpg';
-    return url;
-  }
-
-  function generateComments() {
-    var comments = [];
-    var commentsQuantity = window.assets.getRandomInteger(1, 5);
-
-    for (var i = 0; i < commentsQuantity; i++) {
-      var comment = '';
-      var commentSentences = window.assets.getRandomInteger(1, 2);
-
-      for (var j = 0; j < commentSentences; j++) {
-        var sentence = window.assets.pickRandomArrEl(window.data.COMMENTS);
-        comment += sentence;
-      }
-
-      comments.push(comment);
-    }
-
-    return comments;
-  }
 
   function generatePreviewNode(obj) {
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -42,15 +17,6 @@
     return pictureElement;
   }
 
-  function generatePostData(url) {
-    return {
-      url: url,
-      likes: window.assets.getRandomInteger(15, 200),
-      comments: generateComments(),
-      description: window.assets.pickRandomArrEl(window.data.DESCRIPTIONS)
-    };
-  }
-
   function addPreviewClickListener(link, post) {
     link.addEventListener('click', function (e) {
       e.preventDefault();
@@ -58,17 +24,15 @@
     });
   }
 
-  window.renderPreviews = function () {
+  window.renderPreviews = function (previewList) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < window.data.PICTURES_QUANTITY; i++) {
-      var url = generatePictureUrl(i + 1);
-      var postData = generatePostData(url);
-      otherPeoplePictures.push(postData);
-      var postPreviewNode = generatePreviewNode(postData);
-      addPreviewClickListener(postPreviewNode, postData);
+    previewList.forEach(function (el) {
+      var postPreviewNode = generatePreviewNode(el);
+
+      addPreviewClickListener(postPreviewNode, el);
       fragment.appendChild(postPreviewNode);
-    }
+    });
 
     picturesList.appendChild(fragment);
   };
